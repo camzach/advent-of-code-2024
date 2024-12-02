fc4b773345a7d28eacca0e9358dfabc93a5ae8ef
camzach <22503898+camzach@users.noreply.github.com>
2024-12-02 05:45:46 +0000

    day 2

---
+++ src/day2/mod.rs
@@ -0,0 +1,58 @@
+const INPUT: &str = include_str!("part-1.txt");
+
+fn report_is_safe(report: Vec<i32>, damping: i32) -> bool {
+    let mut is_ascending = false;
+
+    for i in 0..report.len() - 1 {
+        let a = report[i];
+        let b = report[i + 1];
+        if i == 0 && b > a {
+            is_ascending = true;
+        }
+        let diff = a.abs_diff(b);
+        if diff < 1 || diff > 3 || is_ascending != (b > a) {
+            if damping > 0 {
+                for i in 0..report.len() {
+                    let mut clone = report.clone();
+                    clone.remove(i);
+                    if report_is_safe(clone, damping - 1) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+    return true;
+}
+
+pub fn part1() {
+    let reports = INPUT.lines();
+
+    let mut safe_reports = 0;
+    for line in reports {
+        if report_is_safe(
+            line.split(' ').map(|n| n.parse::<i32>().unwrap()).collect(),
+            0,
+        ) {
+            safe_reports += 1;
+        }
+    }
+    println!("Day 2 Part 1: {safe_reports}");
+}
+
+pub fn part2() {
+    let reports = INPUT.lines();
+
+    let mut safe_reports = 0;
+    for line in reports {
+        if report_is_safe(
+            line.split(' ').map(|n| n.parse::<i32>().unwrap()).collect(),
+            1,
+        ) {
+            println!("{line}");
+            safe_reports += 1;
+        }
+    }
+    println!("Day 2 Part 2: {safe_reports}");
+}
