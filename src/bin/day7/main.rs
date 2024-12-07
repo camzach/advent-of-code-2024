fb2b36237bf3879dc3b282cac3598959f2a005c8
camzach <22503898+camzach@users.noreply.github.com>
2024-12-07 07:08:11 +0000

    day 7

--- src/bin/day7/main.rs
+++ src/bin/day7/main.rs
@@ -1,13 +1,76 @@
+use itertools::Itertools;
+
 const INPUT: &str = include_str!("input.txt");
 fn main() {
     println!("Day 7 Part 1: {}", part1());
     println!("Day 7 Part 2: {}", part2());
 }
 
-fn part1() -> u32 {
-    0
+fn part1() -> u64 {
+    let mut total = 0;
+    'line: for line in INPUT.lines() {
+        let (target, numbers) = line.split_once(':').unwrap();
+        let target = target.parse::<u64>().unwrap();
+        let numbers = numbers
+            .trim()
+            .split_whitespace()
+            .map(|n| n.parse::<u64>().unwrap())
+            .collect_vec();
+        let permutations = (0..numbers.len() - 1)
+            .map(|_| ['*', '+'])
+            .multi_cartesian_product()
+            .collect_vec();
+        for perm in permutations {
+            let mut running_result = numbers[0];
+            for (number, operator) in numbers.iter().skip(1).zip(perm) {
+                match operator {
+                    '+' => running_result += number,
+                    '*' => running_result *= number,
+                    _ => unreachable!(),
+                }
+            }
+            if running_result == target {
+                total += target;
+                continue 'line;
+            }
+        }
+    }
+    total
 }
 
-fn part2() -> u32 {
-    0
+fn part2() -> u64 {
+    let mut total = 0;
+    'line: for line in INPUT.lines() {
+        let (target, numbers) = line.split_once(':').unwrap();
+        let target = target.parse::<u64>().unwrap();
+        let numbers = numbers
+            .trim()
+            .split_whitespace()
+            .map(|n| n.parse::<u64>().unwrap())
+            .collect_vec();
+        let permutations = (0..numbers.len() - 1)
+            .map(|_| ['*', '+', '|'])
+            .multi_cartesian_product()
+            .collect_vec();
+        for perm in permutations {
+            let mut running_result = numbers[0];
+            for (number, operator) in numbers.iter().skip(1).zip(perm) {
+                match operator {
+                    '+' => running_result += number,
+                    '*' => running_result *= number,
+                    '|' => {
+                        let digits = number.ilog10() + 1;
+                        running_result *= 10_u64.pow(digits);
+                        running_result += number;
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            if running_result == target {
+                total += target;
+                continue 'line;
+            }
+        }
+    }
+    total
 }
