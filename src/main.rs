cf7f045430e98a23697dd7b85a204bda1cf33379
camzach <22503898+camzach@users.noreply.github.com>
2024-12-07 07:24:32 +0000

    add command to create new days

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use core::str;
-use std::process::Command;
+
+use std::{
+    fs::{create_dir_all, write},
+    path::Path,
+    process::Command,
+};
 
 use chrono::{Datelike, Utc};
 use regex::Regex;
@@ -28,7 +33,6 @@ fn main() {
                 .name("number")
                 .unwrap()
                 .as_str();
-            println!("{day}");
             let mut cmd = Command::new("cargo");
             cmd.args(["run", "--bin", format!("day{day}").as_str()]);
             match cmd.output() {
@@ -37,6 +41,25 @@ fn main() {
                 }
                 Err(_) => println!("Command failed to execute"),
             };
+        }
+        "new" => {
+            let mut cmd = Command::new("sed");
+            let day = Utc::now().naive_local().day();
+            cmd.args([format!("s/%DAY%/{}/", day).as_mut_str(), "template.txt"]);
+            match cmd.output() {
+                Ok(result) => {
+                    let dir_path = format!("./src/bin/day{}", day);
+                    let path = Path::new(dir_path.as_str());
+                    if path.exists() {
+                        println!("Directory for day {day} already exists");
+                        return;
+                    }
+                    create_dir_all(path).expect("Failed to create path");
+                    write(path.join("main.rs"), result.stdout).expect("Failed to write file");
+                    write(path.join("input.txt"), "").expect("Failed to create input.txt");
+                }
+                Err(_) => println!("Failed to fill out template"),
+            }
         }
         els => println!("Unexpected argument {els}"),
     }
